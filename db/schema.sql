3a9ebe550f9e087b9d3fe6a3016fcdb19d19011b
Rachel Wagschal <rawagschal@gmail.com>
2021-02-01 14:34:33 -0800

    edited schema and added seed data

--- db/schema.sql
+++ db/schema.sql
@@ -3,31 +3,30 @@ CREATE DATABASE employees;
 
 USE employees;
 
-CREATE TABLE Department (
-    id 
-        INT 
-        AUTO_INCREMENT 
-        PRIMARY KEY,
-    name 
-        VARCHAR(30) 
-        UNIQUE 
-        NOT NULL
+CREATE TABLE department(
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    name VARCHAR(30) UNIQUE NOT NULL
 );
 
-CREATE TABLE Role (
-    id
-        INT 
-        AUTO_INCREMENT
-        PRIMARY KEY,
-    title
-        VARCHAR(30)
-        UNIQUE
-        NOT NULL,
-    salary
-        DECIMAL
-        NOT NULL,
-    department_id
-        INT
-        NOT NULL,
-    
-)
+CREATE TABLE role(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    FOREIGN KEY (role_id) REFERENCES role(id),
+    manager_id INT,
+    FOREIGN KEY (manager_id) REFERENCES Employee(id)
+);
+
+SELECT role.title, role.salary, department.name
+FROM role
+INNER JOIN department
+ON role.department_id = department.id;
---
+++ db/seed.sql
@@ -0,0 +1,33 @@
+USE employees;
+
+INSERT INTO department (name)
+VALUES 
+    ('C Suite'),
+    ('Sales'),
+    ('Legal'),
+    ('HR'),
+    ('Accounting');
+
+INSERT INTO role (title, salary, department_id)
+VALUES
+    ('CEO', 500000, 1),
+    ('CLO', 400000, 1),
+    ('CFO', 400000, 1),
+    ('Sales Lead', 200000, 2),
+    ('Sales Manager', 150000, 2),
+    ('Sales Assistant', 100000, 2),
+    ('Lawyer', 200000, 3),
+    ('Legal Assistant', 100000, 3),
+    ('Head of HR', 200000, 4),
+    ('HR Manager', 150000, 4),
+    ('HR Assistant', 100000, 4),
+    ('Lead Accountant', 200000, 5),
+    ('Accounting Manager', 150000, 5),
+    ('Accounting Assitant', 100000, 5);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Rachel', 'Wagschal', 1, NULL), 
+    ('Incredible', 'Lawyer', 2, 1);
+
+    
